e675641d1fe4910cf64a3136745688dd423afaae
Ventura <marlenp363@gmail.com>
2022-09-22 21:23:14 -0500

    primero commit

---
+++ Ventura_Semana05/ListaCategoria.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Businness;
+using Entity;
+
+namespace Ventura_Semana05
+{
+    /// <summary>
+    /// Interaction logic for ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria BCategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = BCategoria.Listar(0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs)
+        {
+            ManCategoria manCategoria = new ManCategoria(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)DgvCategoria_SelectionChanged().SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+        
+    }
+}
---
+++ Ventura_Semana05/ManCategoria.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace Ventura_Semana05
+{
+    /// <summary>
+    /// Interaction logic for ManCategoria.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public int ID { get; set; }
+        public ManCategoria(int iD)
+        {
+            InitializeComponent();
+            ID = iD;
+            if (ID > 0)
+            {
+                BCategoria bCategoria = new BCategoria();
+                List<Categoria> categorias = new List<Categoria>();
+                categorias = bCategoria.Listar(ID);
+                if (categorias.Count > 0)
+                {
+                    lblID.Content = categorias[0].IdCategoria.ToString();
+                    txtNombre.Text = categorias[0].NombreCategoria;
+                    txtDescripcion.Text = categorias[0].Descripcion;
+                }
+            }
+        }
+
+        private void BntGrabar_Click(object sender, RouterEventArgs e)
+        {
+            BCategoria Bcategoria = null;
+            bool result = true;
+            try
+            {
+                Bcategoria = new BCategoria();
+                if (ID > 0)
+                    result = Bcategoria.Actualizar(new ManCategoria { IdCategoria = ID, NombreCategoria = txtNombre.Text })
+                else
+                    result = Bcategoria.Insertar(new ManCategoria { NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text })
+
+
+                if (!result)
+                    MessageBox.Show("Comunicarse con el Administrador");
+
+                Close();
+
+            }
+
+            catch(Exception)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+    }
+}
